b03b3c1b21f80095ae4f0ed9bf6a8eaddbb8b9ff
boommanpro <boommanpro@gmail.com>
2026-02-26 22:13:56 +0800

    fix: 将sidepanel.tsx从符号链接改为实际文件内容提交

--- src/sidepanel.tsx
+++ src/sidepanel.tsx
@@ -1,1 +1,25 @@
-/Users/boommanpro/scriptcat/src/pages/sidepanel/index.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ConfigProvider } from "@arco-design/web-react";
+import zhCN from "@arco-design/web-react/es/locale/zh-CN";
+import "@arco-design/web-react/dist/css/arco.css";
+import "./sidepanel.css";
+import { SidePanelApp } from "@App/pages/sidepanel/SidePanelApp.tsx";
+
+function App() {
+  return (
+    <ConfigProvider locale={zhCN}>
+      <SidePanelApp />
+    </ConfigProvider>
+  );
+}
+// log
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  process.env.NODE_ENV === "development" ? (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ) : (
+    <App />
+  )
+);
